0ed2a05e2cb8bf7a834c951cefe1baecb256f0d9
usedtobemeonce <seven.eightm5@gmail.com>
2019-03-30 17:47:53 +0100

    add search

---
+++ src/Settings/Search.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import { backgroundColor2, fontSize2 } from '../Shared/Styles';
+
+const SearchGrid = styled.div`
+    display: grid;
+    grid-template-columns: 200px 1fr;
+`;
+
+const SearchInput = styled.input`
+    ${backgroundColor2};
+    ${fontSize2};
+    height: 25px;
+    color: #1163c9;
+    place-self: center left;
+`;
+
+export default function () {
+    return (<SearchGrid>
+        <h2>Search all coins</h2>
+        <SearchInput />
+    </SearchGrid>)
+}
