5937543dcbaeda009fa70d13d6e29e763f26394c
usedtobemeonce <seven.eightm5@gmail.com>
2019-03-30 20:00:49 +0100

    add highcharts theme

--- src/Dashboard/HighchartsConfig.js
+++ src/Dashboard/HighchartsConfig.js
@@ -1,63 +1,47 @@
 export default function () {
-    return {
-        title: {
-            text: 'Solar Employment Growth by Sector, 2010-2016'
-        },
+  return {
+    title: {
+      text: ''
+    },
 
-        subtitle: {
-            text: 'Source: thesolarfoundation.com'
-        },
+    yAxis: {
+      title: {
+        text: 'Price'
+      }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle'
+    },
 
-        yAxis: {
-            title: {
-                text: 'Number of Employees'
-            }
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false
         },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
+        pointStart: 2010
+      }
+    },
+
+    series: [{
+      name: 'Installation',
+      data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    },],
+
+    responsive: {
+      rules: [{
+        condition: {
+          maxWidth: 500
         },
-
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
-        },
-
-        series: [{
-            name: 'Installation',
-            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-        }, {
-            name: 'Manufacturing',
-            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-        }, {
-            name: 'Sales & Distribution',
-            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-        }, {
-            name: 'Project Development',
-            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-        }, {
-            name: 'Other',
-            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-        }],
-
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
+        chartOptions: {
+          legend: {
+            layout: 'horizontal',
+            align: 'center',
+            verticalAlign: 'bottom'
+          }
         }
+      }]
     }
+  }
 }
--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import highchartsCofnig from './HighchartsConfig';
-import { Tile } from '../Shared/Tile';
-import { AppContext } from '../App/AppProvider';
-import ReactHighCharts from 'react-highcharts';
+import highchartsConfig from './HighchartsConfig';
+import { Tile } from "../Shared/Tile";
+import { AppContext } from "../App/AppProvider";
+import ReactHighcharts from 'react-highcharts';
+import HighchartsTheme from './HighchartsTheme';
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 
 export default function () {
     return (
         <AppContext.Consumer>
             {() =>
                 <Tile>
-                    <ReactHighCharts config={highchartsCofnig()} />
+                    <ReactHighcharts config={highchartsConfig()} />
                 </Tile>
             }
         </AppContext.Consumer>
